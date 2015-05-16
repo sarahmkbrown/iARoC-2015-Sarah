493627ed461b728cb126cd7391554f6a6deee2e8
sarahmkbrown <Guest@WTS6.local>
2015-05-16 15:11:46 -0700

    Added “LOADED” comment

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -42,6 +42,7 @@ public class Brain extends IRobotCreateAdapter {
 	}
 
 	public void initialize() throws ConnectionLostException {
+        //LOADED
 		dashboard.log("iARoC-2015");
 		driveDirect(wheelSpeed, wheelSpeed);
 	}
